86f1babb9c0cef90b5202eb0125b1910377e3a15
persistank <hgbekrar@gmail.com>
2026-04-09 15:02:24 +0200

    backend fastapi works with one function to compute persistance with gudhi

---
+++ backend/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import gudhi
+import numpy as np
+
+#necessaire pour des raisons de sécurité
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title = 'persistan.fr API', description='moteur de calcul topologique interactif')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class NuageDePoints(BaseModel):
+    points: list[list[float]]
+
+
+@app.post("/calculer_persistance")
+async def calcul_persisance(donnees:NuageDePoints):
+    points_np = np.array(donnees.points)
+
+    alpha_complex = gudhi.AlphaComplex(points = points_np)
+    simplex_tree = alpha_complex.create_simplex_tree()
+    simplex_tree.compute_persistence()
+
+    diagramme_brut = simplex_tree.persistence()
+
+    diagramme_propre = []
+
+    for dim,(b,d) in diagramme_brut:
+        if d != float('inf'):
+            diagramme_propre.append({
+                "dim": dim,
+                "birth": float(np.sqrt(b)), 
+                "death": float(np.sqrt(d))
+            })
+    return {"diagramme": diagramme_propre}
+
