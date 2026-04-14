2554d4203c8ee183a8370f664240aba3f6b25e4c
persistank <hgbekrar@gmail.com>
2026-04-14 14:11:44 +0200

    added canvas and setup the arrays in the js file to do the logic for the filtration

--- frontend/script.js
+++ frontend/script.js
@@ -5,3 +5,45 @@ function afficherPage(idPage){
     const pageDemandee = document.getElementById(idPage);
     pageDemandee.style.display = 'block';
 }
+
+var canvas_filtration = document.getElementById("filtration");
+var ctx_filtration = canvas_filtration.getContext("2d");
+ctx_filtration.fillStyle = "black";
+ctx_filtration.fillRect(10, 10, 500, 500);
+
+/* valeur dans la filtration et tableau a inserer dans la filtration gudhi et position "couple (x,y)" 
+par exemple sommets[0] = (1, [0], (10.0,11.0))*/
+let sommets = [];
+let arretes = [];
+let triangles = [];
+
+let filtration = [];
+
+let sommets_courant = [];
+
+function ajouterSimplexe(){
+    value = "";
+    if (value=="0"){
+        
+    }
+    else if (value =="1"){
+
+    }
+    else if (value=="2"){
+
+    }
+}
+
+function calculPlusProcheVoisin(x_new,y_new){
+    if (sommets.length()!=0){
+        let min_dist = (x_new-sommets[0][0])^2 + (y_new-sommets[0][1])^2;
+        let argmin=0;
+        for ((x,y) of sommets){
+            let dist = (x_new-x)^2 + (y_new-y)^2;
+            if (dist < min_dist){
+                argmin = 
+            }
+        }
+    }
+}
+
